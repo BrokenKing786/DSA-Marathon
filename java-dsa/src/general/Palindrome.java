9a639545a307e87aa275f00e4fe679ef36819a02
abhinav <krishnanabhinav6@gmail.com>
2026-01-25 14:52:44 +0530

    palindrome of number

---
+++ java-dsa/src/general/Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        int n = 65;
+        int divisor = 0;
+        int rev = 0;
+
+        while (n != 0) {
+            divisor = n % 10;
+            rev = rev * 10 + divisor;
+            n = n / 10;
+        }
+
+        System.out.println(rev);
+    }
+}

08317a9ac4ee2d7ca63f5a3325e72345103ce85d
abhinav <krishnanabhinav6@gmail.com>
2026-04-25 00:31:24 +0530

    selection Sort

---
+++ java-dsa/src/arrays/SelectionSort.java
@@ -0,0 +1,27 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int arr[] = { 11, 101, 22, 5, 4 };
+        int minIndex = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
